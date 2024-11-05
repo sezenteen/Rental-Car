49e4f03d936f432d693b2e6544bbaf7e8febff54
sezenteen <sezenteen@gmail.com>
2024-11-05 23:44:35 +0800

    user page need to work on it more

--- src/main/java/com/example/rentalcar/controller/CarController.java
+++ src/main/java/com/example/rentalcar/controller/CarController.java
@@ -1,4 +1,23 @@
 package com.example.rentalcar.controller;
 
+import com.example.rentalcar.repository.BrandRepository;
+import com.example.rentalcar.repository.CarRepository;
+import com.example.rentalcar.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CarController {
+    private final BrandRepository brandRepository;
+    private CarRepository carRepository;
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService, CarRepository carRepository, BrandRepository brandRepository) {
+        this.carService = carService;
+        this.carRepository = carRepository;
+        this.brandRepository = brandRepository;
+    }
+
+
 }
